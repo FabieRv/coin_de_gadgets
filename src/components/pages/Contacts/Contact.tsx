efccf34d86f1186e84ceb35b28df5a64c1a07157
fabierv <fabie.rav@gmail.com>
2025-12-16 16:17:09 +0300

    contact

--- src/components/pages/Contacts/Contact.tsx
+++ src/components/pages/Contacts/Contact.tsx
@@ -1,5 +1,105 @@
+import Button from "../../common/Button"
+import Container from "../../common/Container"
+import { FaRunning, FaPhone, FaMapMarkerAlt } from "react-icons/fa"
+
 function Contact() {
-  return <div>Contact</div>
+  const iconStyle = "text-white text-3xl"
+  const infoBlockClasses =
+    "flex flex-col items-center text-center p-6 space-y-3"
+
+  return (
+    <div className="relative ">
+      <div className="absolute w-full h-[200px] left-0 bottom-0 bg-[#145aaf]"></div>
+      <Container>
+        {" "}
+        <div className="flex flex-col items-center gap-2">
+          <h1 className="text-3xl font-bold text-gray-800">Contactez-nous</h1>
+          <p className="text-lg text-gray-700 text-center px-4  font-lato">
+            Des questions ou des remarques?
+          </p>
+          <p className="text-lg text-gray-700 text-center px-4  font-lato">
+            {" "}
+            Écrivez-nous simplement un message !
+          </p>
+        </div>
+        <form className="max-w-xl mx-auto space-y-4 mt-4">
+          <div className="flex flex-col md:flex-row justify-between gap-4 ">
+            <div className="flex flex-col w-full md:w-1/2 gap-2 mt-2">
+              {/* Input Name */}
+              <label htmlFor="">name</label>
+              <input
+                type="text"
+                placeholder="Enter your Name"
+                className="p-3 bg-gray-100 rounded-lg w-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#50b4b4]"
+              />
+            </div>
+            {/* Input email*/}
+            <div className="flex flex-col w-full md:w-1/2 gap-2 mt-2">
+              <label htmlFor="">Email</label>
+              <input
+                type="email"
+                placeholder="Enter a valid email address"
+                className="p-3 bg-gray-100 rounded-lg w-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#50b4b4]"
+              />
+            </div>
+          </div>
+
+          {/* Bouton Soumettre */}
+          <div className="flex justify-center">
+            <Button
+              label="  SOUMETTRE"
+              className="w-full md:w-full py-3 text-lg font-semibold "
+            ></Button>
+          </div>
+        </form>
+        <div className="bg-gray-100 mt-12 py-10">
+          <div className="flex flex-col md:flex-row justify-evenly items-start">
+            <div className={infoBlockClasses}>
+              <div className="bg-primary p-4 rounded-full">
+                <FaRunning className={iconStyle} />
+              </div>
+              <h2 className="text-xl font-bold uppercase font-Montser text-gray-800">
+                À PROPOS DU CLUB
+              </h2>
+              <div className="text-gray-600">
+                <p>Vente de produits</p>
+                <p>Reduction livraison +50%</p>
+              </div>
+            </div>
+
+            {/* Bloc 2: TÉLÉPHONE (FIXE) */}
+            <div className={infoBlockClasses}>
+              <div className="bg-primary p-4 rounded-full">
+                <FaPhone className={iconStyle} />
+              </div>
+              <h2 className="text-xl font-bold uppercase font-Montser text-gray-800">
+                TÉLÉPHONE (FIXE)
+              </h2>
+              <div className="text-gray-600">
+                <p>+264 35 2657 8987</p>
+                <p>+ 2 262 3336</p>
+              </div>
+            </div>
+
+            {/* Bloc 3: EMPLACEMENT DE NOTRE BUREAU */}
+            <div className={infoBlockClasses}>
+              <div className="bg-primary p-4 rounded-full">
+                <FaMapMarkerAlt className={iconStyle} />
+              </div>
+              <h2 className="text-xl font-bold uppercase font-Montser text-gray-800">
+                ADRESSE DE NOTRE BUREAU
+              </h2>
+              <div className="text-gray-600 text-sm">
+                <p>Gadgets online</p>
+                <p>Sabotsy Namehana local</p>
+                <p>Antananarivo, 101</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  )
 }
 
 export default Contact
--- src/components/pages/Gallery/Gallery.tsx
+++ src/components/pages/Gallery/Gallery.tsx
@@ -17,39 +17,6 @@ const Gallery = () => {
             Découvrez nos gadgetsv
           </h2>
 
-          {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-            {galleryImages.map((image) => (
-              <div
-                key={image.id}
-                className="relative w-full overflow-hidden rounded-lg "
-              >
-                <div className="pt-[100%]"></div>{" "}
-                <img
-                  src={image.src}
-                  alt={image.alt}
-                  className="absolute top-0 left-0 w-full h-full object-cover p-4
-                           transform group-hover:scale-105 transition-transform duration-300"
-                />
-                <div
-                  className="absolute inset-0 bg-zinc-300 bg-opacity-40 flex items-center justify-center 
-                              opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-                >
-                  <div className="text-white text-lg font-semibold">
-                    <h1 className="text-black text-center font-bold">
-                      Motos Pot GeaForce RTX
-                    </h1>
-                    <p className="text-black text-center p-8">
-                      Saepe nihil, corporis in maiores, dignissimos temporibus
-                      distinctio reprehenderit unde ipsa ex possimus, nesciunt
-                      minus odit tempore sequi facilis amet laudantium
-                      laboriosam.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div> */}
-
           <Masonry
             breakpointCols={breakpointColumnsObj}
             className="flex gap-2 mt-10"
@@ -60,7 +27,7 @@ const Gallery = () => {
                 key={i}
                 src={item.image}
                 alt={`Gallery pics ${i + 1}`}
-                className={`w-full rounded overflow-hidden object-cover cursor-pointer  border-6 border-[#50B4B4] ${item.height} `}
+                className={`w-full h-full  object-cover p-2 overflow-hidden cursor-pointer  border-4 border-[#50B4B4] ${item.height} `}
                 style={{ objectFit: "cover" }}
                 loading="lazy"
               />
